29dfdac6754061fa47b0e906484947a747083300
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-04-10 01:51:48 +0200

    Improved install script, statically link granite

--- meson/post_install.py
+++ meson/post_install.py
@@ -2,9 +2,23 @@
 
 import os
 import subprocess
+import platform
 
-schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+if platform.processor() == "arm":
+    homebrewdir = os.path.join('/', 'opt', 'homebrew')
+else:
+    homebrewdir = os.path.join('/', 'usr', 'local')
 
-if not os.environ.get('DESTDIR'):
-    print('Compiling gsettings schemas…')
-    subprocess.call(['glib-compile-schemas', schemadir])
+
+schemadir = os.path.join(homebrewdir, 'share', 'glib-2.0', 'schemas');
+iconsdir = os.path.join(homebrewdir, 'share', 'icons');
+
+os.system('cp ../schema/* ' + schemadir)
+os.system('cp -r ../icons/elementary ' + iconsdir + '/elementary')
+os.system('cp ../meson/settings.ini ' + '~/.config/gtk-3.0/settings.ini')
+
+os.system('cp ../build/src/io.elementary.code ' + '../application/Code.app/Contents/MacOS/io.elementary.code')
+os.system('cp -r ../application/Code.app ' + '/Applications/Code.app')
+
+subprocess.call(['glib-compile-schemas', schemadir])
+subprocess.call('gtk-update-icon-cache')
